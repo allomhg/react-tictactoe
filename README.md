4a2ec6b7fc435062db1a223922b5e85edceb3f36
allomhg <allomhg@gmail.com>
2023-08-27 17:24:59 +0100

    Trying to do extra question 2

--- README.md
+++ README.md
@@ -4,6 +4,7 @@ As of 27/08/2023 this tutorial is complete, excluding the bonus tasks.
 
 Bonus tasks to be implemented (in order of increasing difficulty):
   1.  - [x] For the current move only, show “You are at move #…” instead of a button.  
+              - This was done using a ternary operator to conditionally render the items in the game history list.
   2.  - [ ] Rewrite Board to use two loops to make the squares instead of hardcoding them.
   3.  - [ ] Add a toggle button that lets you sort the moves in either ascending or descending order.
   4.  - [ ] When someone wins, highlight the three squares that caused the win (and when no one wins, display a message about the result being a draw).
--- src/App.js
+++ src/App.js
@@ -1,14 +1,7 @@
 import React from "react";
 import { useState } from "react";
 
-function Square ({ value, onSquareClick }) {  
-  return (
-    <button className="square" onClick={onSquareClick}>{value}</button>
-  );
-}
-
 export default function Game() {
-  
   const [history, setHistory] = useState([Array(9).fill(null)]);
   const [currentMove, setCurrentMove] = useState(0);
   const xIsNext = currentMove % 2 === 0;
@@ -80,25 +73,66 @@ function Board({ xIsNext, squares, onPlay }) {
     status = "Next player: " + (xIsNext ? "X" : "O");
   }
 
+  function BoardGrid() {
+    let squaresCount = 0;
+    let boardArr= [];
+    
+    for (let i = 0; i < 3; i++) {
+      let squareArr = [];
+      for (let j = 0; j < 3; j++) {
+        squareArr.push(
+          <Square 
+            value={squares[squaresCount]} 
+            onSquareClick={() => handleClick(squaresCount)} 
+          />
+        );
+        squaresCount++;
+        boardArr.push(squareArr);
+      }
+
+      return (
+        <div className="board-row">
+          {
+            boardArr.map( (item) => {
+              console.log(item);
+              return item;
+            })
+          }
+        </div>
+      );
+    }
+  }
+
   return (
     <div>
       <div className="status">{status}</div>
-      <div className="board-row">
-        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-        <Square value={squares[1]} onSquareClick={() => handleClick(1)}/>
-        <Square value={squares[2]} onSquareClick={() => handleClick(2)}/>
-      </div>
-      <div className="board-row">
-        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
-        <Square value={squares[4]} onSquareClick={() => handleClick(4)}/>
-        <Square value={squares[5]} onSquareClick={() => handleClick(5)}/>
-      </div>
-      <div className="board-row">
-        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
-        <Square value={squares[7]} onSquareClick={() => handleClick(7)}/>
-        <Square value={squares[8]} onSquareClick={() => handleClick(8)}/>
-      </div>
+      <BoardGrid/>
     </div>
+
+    // <div>
+    //   <div className="status">{status}</div>
+    //   <div className="board-row">
+    //     <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+    //     <Square value={squares[1]} onSquareClick={() => handleClick(1)}/>
+    //     <Square value={squares[2]} onSquareClick={() => handleClick(2)}/>
+    //   </div>
+    //   <div className="board-row">
+    //     <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+    //     <Square value={squares[4]} onSquareClick={() => handleClick(4)}/>
+    //     <Square value={squares[5]} onSquareClick={() => handleClick(5)}/>
+    //   </div>
+    //   <div className="board-row">
+    //     <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+    //     <Square value={squares[7]} onSquareClick={() => handleClick(7)}/>
+    //     <Square value={squares[8]} onSquareClick={() => handleClick(8)}/>
+    //   </div>
+    // </div>
+  );
+}
+
+function Square ({ value, onSquareClick }) {  
+  return (
+    <button className="square" onClick={onSquareClick}>{value}</button>
   );
 }
 

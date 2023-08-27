3437a48f6e3da43ec99f279a4ba02353d19e80f6
allomhg <allomhg@gmail.com>
2023-08-27 15:09:47 +0100

    Solved the first extra task

--- src/App.js
+++ src/App.js
@@ -26,14 +26,22 @@ export default function Game() {
 
   const moves = history.map((squares, move) => {
     let description;
-    if (move > 0) {
+
+    if (move === currentMove) {
+      description = "Current move";
+    } else if (move > 0) {
       description = "Go to move #" + move;
     } else {
       description = "Go to game start";
     }
+
     return (
       <li key={move}>
-        <button onClick={() => jumpTo(move)}>{description}</button>
+        {
+          move === currentMove ? 
+            <div>{description}</div> :
+            <button onClick={() => jumpTo(move)}>{description}</button>     
+        }
       </li>
     );
   });

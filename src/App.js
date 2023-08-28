217df6745ebf2b7f7a3ac78fe9df2c36f246389c
allomhg <allomhg@gmail.com>
2023-08-28 18:57:00 +0100

    Not working

--- src/App.js
+++ src/App.js
@@ -73,60 +73,78 @@ function Board({ xIsNext, squares, onPlay }) {
     status = "Next player: " + (xIsNext ? "X" : "O");
   }
 
-  function BoardGrid() {
-    let squaresCount = 0;
-    let boardArr= [];
+  // function BoardGrid() {
+  //   let squaresCount = 0;
+  //   let boardArr= [];
     
-    for (let i = 0; i < 3; i++) {
-      let squareArr = [];
-      for (let j = 0; j < 3; j++) {
-        squareArr.push(
-          <Square 
-            value={squares[squaresCount]} 
-            onSquareClick={() => handleClick(squaresCount)} 
-          />
-        );
-        squaresCount++;
-        boardArr.push(squareArr);
-      }
+  //   for (let i = 0; i < 3; i++) {
+  //     let squareArr = [];
+  //     for (let j = 0; j < 3; j++) {
+  //       squareArr.push(
+  //         <Square 
+  //           value={squares[squaresCount]} 
+  //           onSquareClick={() => handleClick(squaresCount)} 
+  //         />
+  //       );
+  //       squaresCount++;
+  //       boardArr.push(squareArr);
+  //     }
 
-      return (
-        <div className="board-row">
-          {
-            boardArr.map( (item) => {
-              console.log(item);
-              return item;
-            })
-          }
-        </div>
-      );
-    }
-  }
+  //     return (
+  //       <div className="board-row">
+  //         {
+  //           boardArr.map( (item) => {
+  //             console.log(item);
+  //             return item;
+  //           })
+  //         }
+  //       </div>
+  //     );
+  //   }
+  // }
+
+  // let squareCount = 0;
+
+  // const boardRow = () => {
+  //   for (let j = 0; j < 3; j++) {
+  //     <Square value={squares[sq]} onSquareClick={() => handleClick(0)} />
+  //     squareCount++;
+  //   }
+  // }
+
+  // const boardGrid = () => {
+  //   const boardGrids = [];
+  //   for (let i = 0; i < 3; i++) {
+  //     boardGrids.push(<div className="board-row">{boardRow}</div>)
+  //   }
+  // }
 
   return (
-    <div>
-      <div className="status">{status}</div>
-      <BoardGrid/>
-    </div>
-
     // <div>
     //   <div className="status">{status}</div>
-    //   <div className="board-row">
-    //     <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-    //     <Square value={squares[1]} onSquareClick={() => handleClick(1)}/>
-    //     <Square value={squares[2]} onSquareClick={() => handleClick(2)}/>
-    //   </div>
-    //   <div className="board-row">
-    //     <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
-    //     <Square value={squares[4]} onSquareClick={() => handleClick(4)}/>
-    //     <Square value={squares[5]} onSquareClick={() => handleClick(5)}/>
-    //   </div>
-    //   <div className="board-row">
-    //     <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
-    //     <Square value={squares[7]} onSquareClick={() => handleClick(7)}/>
-    //     <Square value={squares[8]} onSquareClick={() => handleClick(8)}/>
-    //   </div>
+    //   {
+        
+    //   }
     // </div>
+
+    <div>
+      <div className="status">{status}</div>
+      <div className="board-row">
+        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+        <Square value={squares[1]} onSquareClick={() => handleClick(1)}/>
+        <Square value={squares[2]} onSquareClick={() => handleClick(2)}/>
+      </div>
+      <div className="board-row">
+        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+        <Square value={squares[4]} onSquareClick={() => handleClick(4)}/>
+        <Square value={squares[5]} onSquareClick={() => handleClick(5)}/>
+      </div>
+      <div className="board-row">
+        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+        <Square value={squares[7]} onSquareClick={() => handleClick(7)}/>
+        <Square value={squares[8]} onSquareClick={() => handleClick(8)}/>
+      </div>
+    </div>
   );
 }
 
